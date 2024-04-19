fe6688a0234d5eb682f6b31409c3690f2d18177b
Jayanta <bjayanta.me@gmail.com>
2024-04-19 11:17:15 +0600

    auth is done

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { AuthPayloadDto } from './dto/auth.dto';
+import { JwtService } from '@nestjs/jwt';
+
+const fakeUsers = [
+  { id: 1, username: 'anson', password: '12345678' },
+  { id: 2, username: 'jack', password: 'password' },
+];
+
+@Injectable()
+export class AuthService {
+  constructor(private jwtService: JwtService) {}
+
+  validateUser({ username, password }: AuthPayloadDto) {
+    const findUser = fakeUsers.find((user) => user.username === username);
+    if (!findUser) return null;
+
+    if (password === findUser.password) {
+      const { password, ...user } = findUser;
+      return this.jwtService.sign(user);
+    }
+  }
+}
