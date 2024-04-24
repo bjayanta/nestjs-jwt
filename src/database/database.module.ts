939f2afc641402ce67668cdd0db8192645fdbdab
Jayanta <bjayanta.me@gmail.com>
2024-04-24 12:51:45 +0600

    create user entity

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -14,6 +14,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         password: config.getOrThrow<string>('DB_PASSWORD'),
         synchronize: config.getOrThrow<boolean>('DB_SYNCHRONIZE'),
         autoLoadEntities: true,
+        // logging: true,
       }),
       inject: [ConfigService],
     }),
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,48 @@
-export class User {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn({ type: 'bigint', unsigned: true })
+  id: number;
+
+  @Column({ length: 100 })
+  name: string;
+
+  @Column({ length: 100, unique: true })
+  email: string;
+
+  @Column({ type: 'timestamp', default: null })
+  email_verified_at: Date;
+
+  @Column()
+  password: string;
+
+  @Column({ default: false })
+  is_superadmin: boolean;
+
+  @Column({ length: 100 })
+  remember_token: string;
+
+  @CreateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+  })
+  created_at: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    onUpdate: 'CURRENT_TIMESTAMP(6)',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+  })
+  updated_at: Date;
+
+  constructor(user: Partial<User>) {
+    Object.assign(this, user);
+  }
+}
